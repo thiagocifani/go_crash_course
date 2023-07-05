0cb0c26d20202ef3b47e004d573cb74d910ae7da
thiagocifani <cifani.thiago@gmail.com>
2023-07-04 23:46:17 -0300

    Playing with variables in Go

---
+++ 02_vars/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// MAIN TYPES
+	// string
+	// bool
+	// int
+	// int int8 int16 int32 int64
+	// uint uint8 uint16 uint32 uint64 uintptr
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32 float64
+	// complex64 complex128
+
+	// Using var
+	var name string = "Thiago"
+	var age int = 34
+	var isCool = true
+	isCool = false
+
+	fmt.Println(name, age, isCool)
+	fmt.Printf("%T\n", isCool)
+}
