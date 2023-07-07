94a006f4ec7a4b93b33ccba7ff819745acdea860
thiagocifani <cifani.thiago@gmail.com>
2023-07-07 00:01:50 -0300

    Learning conditionals

---
+++ 06_conditionals/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 15
+	y := 10
+
+	// if else
+	if x <= y {
+		fmt.Printf("%d is less than %d\n", x, y)
+	} else {
+		fmt.Printf("%d is greater than %d\n", y, x)
+	}
+
+	// else if
+
+	color := "red"
+
+	if color == "red" {
+		fmt.Println("color is red")
+	} else if color == "blue" {
+		fmt.Println("color is blue")
+	} else {
+		fmt.Println("color is not blue or red")
+	}
+
+	// Switch
+
+	switch color {
+	case "red":
+		fmt.Println("color is red")
+	case "blue":
+		fmt.Println("color is blue")
+	default:
+		fmt.Println("color is not red or blue")
+	}
+}
