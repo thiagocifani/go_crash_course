89ebb5f51de7fab085fa5c2c0228f1c343d516dc
thiagocifani <cifani.thiago@gmail.com>
2023-07-06 23:48:55 -0300

    Add getSum function to the party

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -6,6 +6,11 @@ func gretting(name string) string {
 	return "Hello " + name
 }
 
+func getSum(num1, num2 int) int {
+	return num1 + num2
+}
+
 func main() {
 	fmt.Println(gretting("Thiago"))
+	fmt.Println(getSum(3, 4))
 }
